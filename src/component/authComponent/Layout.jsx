6d8636abaaf29b87ceeb3262b4e5a7dcf1499f5b
crashmovies <swarnakarharshendu420@gmail.com>
2025-09-10 01:36:12 +0530

    user

--- src/component/authComponent/Layout.jsx
+++ src/component/authComponent/Layout.jsx
@@ -34,7 +34,7 @@ const AuthLayout = () => {
 
         {/* Right: About Us */}
         <button
-          className="bg-primary text-white px-4 py-2 rounded-3xl hover:bg-white hover:text-primary border border-primary flex flex-row gap-2"
+          className="bg-primary text-white px-4 py-2 rounded-3xl border border-primary flex flex-row gap-2 hover:bg-blue-800"
           onClick={() => navigate('/shop/about')}
         >
           <span>About Us</span>
